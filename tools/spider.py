7a73b7ae4f4315447941945548bc06b919fe51a3
MiguelMJ <miguelmejiajimenez@hotmail.com>
2020-12-15 10:49:46 +0100

    feat: Make spider respect robots.txt

--- tools/spider.py
+++ tools/spider.py
@@ -1,8 +1,12 @@
 import requests
+from urllib.robotparser import RobotFileParser
+from urllib.parse import urlparse
 from random import random as rnd
 from time import sleep
 
 import pathlib
+
+rp = {}
 
 class FalseResponse:
     def __init__(self,code,content):
@@ -19,6 +23,16 @@ def get(url, cache=True):
         return FalseResponse(200,res)
     
     p.parent.mkdir(parents=True, exist_ok=True)
+    # Check the robot.txt
+    url_struct = urlparse(url)
+    base = url_struct.netloc
+    if base not in rp:
+        url_struct.path = 'robots.txt'
+        rp[base] = RobotFileParser()
+        rp[base].set_url(url_struct.geturl())
+        rp.read()
+    if not rp[base].can_fetch(url):
+        return FalseResponse(403,'robots.txt forbids it')
     # Or make the petition
     t = rnd()*5+2
     print('[\033[38;2;250;250;0m{:4.2f}\033[0m] {}'.format(t, url))
