3e9b62af4c8ab49509584e030af8121244a7ae72
MiguelMJ <miguelmejiajimenez@hotmail.com>
2020-12-15 12:36:37 +0100

    feat: Display answerws with colored values

--- tools/displayer.py
+++ tools/displayer.py
@@ -1,3 +1,5 @@
+from tools.analyzer import word_expected_reputation
+
 red   = (250,0,0)
 green = (0,250,0)
 blue  = (0,0,250)
@@ -12,6 +14,24 @@ gray2 = (150,150,150)
 gray3 = (100,100,100)
 gray4 = (50,50,50)
 black = (0,0,0)
+
+def lighten(c, r):
+    dr = (250-c[0])*r
+    dg = (250-c[1])*r
+    db = (250-c[2])*r
+    return (int(c[0]+dr),int(c[1]+dg),int(c[2]+db))
+
+def darken(c, r):
+    dr = c[0]*r
+    dg = c[1]*r
+    db = c[2]*r
+    return (int(c[0]-dr),int(c[1]-dg),int(c[2]-db))
+
+def interpolate(c,d,r):
+    dr = (d[0]-c[0])*r
+    dg = (d[1]-c[1])*r
+    db = (d[2]-c[2])*r
+    return (int(c[0]+dr),int(c[1]+dg),int(c[2]+db))
 
 def bold(msg):
     return '\033[1m{}\033[0m'.format(msg)
@@ -75,4 +95,21 @@ def disp_word_freq(word_info, limit=10):
     if len(word_info) > limit:
         word_info=word_info[:limit]
     print('\n'.join(str((x.word, x.frequency)) for x in word_info))
+    
+def disp_answer_rated(answer, word_info, tag_info):
+    all_words = set(answer.qBody.split())
+    word_value = dict()
+    for w in all_words:
+        v = word_expected_reputation(w,word_info,tag_info)
+        word_value[w] = v
+    max_value = max(word_value.values())
+    ll = [x.split() for x in answer.qBody.split('\n')]
+    print(bold(answer.summary.title))
+    for l in ll:
+        for w in l:
+            rel_value = word_value[w]/max_value
+            col = gray3 if rel_value == 0 else interpolate(darken(yellow,0.25),magenta, rel_value)
+            print(fg(w,col)+' ',end='')
+        print()
+    print()
     
