040fe223d1ac0e6ac610b3ca087bc643944beb91
MiguelMJ <miguelmejiajimenez@hotmail.com>
2020-12-22 12:08:11 +0100

    feat: First version of the argument parser

--- answerable.py
+++ answerable.py
@@ -1,48 +1,100 @@
 import sys
+import json
+import argparse
+import textwrap
 
 from tools import fetcher, displayer, analyzer
 
+def save(args):
+    with open(".config","w") as fh:
+        tags = fetcher.get_user_tags(args.tags_file)
+        json.dump({
+                "user": args.user,
+                "tags": tags
+            },
+            fh)
 
-if len(sys.argv) < 2 or len(sys.argv) > 3:
-    print("usage: python answerable.py <user_id> [summary|tags|answers|usertags]")
-    exit()
+def summary():
+    pass
 
-user_id = sys.argv[1]
-option = sys.argv[2] if len(sys.argv) == 3 else "questions"
-ans = fetcher.get_answers(user_id)
-tag_info = analyzer.tag_info(ans)
-word_info = analyzer.word_info(ans)
+def tags():
+    pass
 
-if option == "summary":
-    displayer.disp_summary([x.summary for x in ans])
-elif option == "tags":
-    displayer.disp_tags(list(tag_info.values()))
-elif option == "answers":
-    for a in ans:
-        displayer.disp_answer_rated(a, word_info, tag_info)
-elif option == "questions":
-    questions = fetcher.get_tagged_question_feed(["c++", "java", "python"])
-    vq = [
-        (analyzer.answer_expected_reputation(q, word_info, tag_info), q)
-        for q in questions
-    ]
-    vq.sort(reverse=True)
-    displayer.disp_questions([q for (v, q) in vq[:10]])
-elif option == "usertags":
-    t = fetcher.get_user_tags("source.html")
-    print("FOLLOWED")
-    print("\n".join(t["followed"]))
-    print("IGNORED")
-    print("\n".join(t["ignored"]))
-else:
-    print("Unknown option", option)
+def answers():
+    pass
 
-# Code to compare expected vs real reputation
-#
-# ans.sort(key=lambda x:(x.summary.reputation,x.summary.title), reverse=True)
-# reality = [(a.summary.title[:25],a.summary.reputation) for a in ans]
-# expectations = [(analyzer.answer_expected_reputation(x,word_info,tag_info),x.summary.title) for x in ans]
-# expectations.sort(reverse=True)
-# expectations = [(y[:25],x) for (x,y) in expectations]
-# for x in zip(reality,expectations):
-#    print(x)
+def questions():
+    pass
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+            description="Stack Overflow unanswered questions recommendation system",
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            epilog=textwrap.dedent("""\
+                For more information, see the README.md
+                The code for Answerable is available in https://github.com/MiguelMJ/Answerable
+                """)
+        )
+    
+    parser.add_argument("command",
+                        choices=(
+                            "save",
+                            "summary",
+                            "tags",
+                            "answers",
+                            "questions"
+                        ),
+                        help="save,summary,tags,answers,questions", 
+                        metavar="COMMAND")
+    parser.add_argument("-r","--reverse", 
+                        help="reverse sorting", 
+                        action="store_true")
+    parser.add_argument("-u","--user", 
+                        help="identifier of Stack Overflow user",
+                        metavar="ID")
+    parser.add_argument("-l","--limit", 
+                        help="limit the number of items displayed",
+                        type=int,
+                        default=15,
+                        metavar="N")
+    parser.add_argument("-m","--max-line", 
+                        help="truncate titles with a max length in <summary>",
+                        type=int,
+                        default=80,
+                        metavar="N")
+    parser.add_argument("-k","--key", 
+                        help="sort the items displayed to a given key. It has no effect on <questions>",
+                        choices=(
+                            "date",
+                            "count",
+                            "score",
+                            "value",
+                            "reputation"
+                        ),
+                        default="date",
+                        metavar="K")
+    parser.add_argument("-a","--all", 
+                        help="search through all the new questions, without tag filter. If the user tags haven't been saved before with the <save> command, this option is on by default.",
+                        action="store_true")
+    parser.add_argument("-t","--tags",
+                        help="file with the source of the page with the user followed and ignored tags",
+                        metavar="FILE")
+    parser.add_argument("--no-ansi", 
+                        help="print without colors",
+                        action="store_true")
+    args = parser.parse_args()
+    if args.no_ansi:
+        displayer.ansi = False
+    return args
+
+if __name__ == "__main__":
+    switch = {
+        "save":save,
+        "summary":summary,
+        "tags":tags,
+        "answers":answers,
+        "questions":questions,
+    }
+    args = parse_arguments()
+    command = args.command
+    switch[command](args)
