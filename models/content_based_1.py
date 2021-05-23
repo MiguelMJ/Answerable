cc9bde62d04f9710f05199247af4b5968acb00d0
MiguelMJ <miguelmejiajimenez@hotmail.com>
2021-05-23 16:43:26 +0200

    updating model (incomplete)

--- models/content_based_1.py
+++ models/content_based_1.py
@@ -42,43 +42,40 @@ def recommend(user_qa, feed):
 
     nans = len(answered)
 
-    """
-    The following code is an adapted version of the Content-Based recommmender
-    described in this tutorial:
-
-    https://www.datacamp.com/community/tutorials/recommender-systems-python
-    """
-
     tfidf = TfidfVectorizer(stop_words="english")
 
-    # list of vectorized body and tags
+    # list of vectorized text
     tfidf_matrix = tfidf.fit_transform(answered + unanswered)
 
-    # similarity matrices: without and with tags
+    # similarity matrix of each answer with the rest
     cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
     # rows: unanswered, cols: answered
     unans_similarity = cosine_sim[nans:, :nans]
 
-    # form of the following list: [(feed index, value)], where value is the
-    # value of the maximum similarity with a single answered question
+    # index: unanswered. values: max similarity and score
     max_sim = list(enumerate([max(r) for r in unans_similarity]))
-
+    score = [x * len(unanswered[i].split()) for i,x in max_sim]
+    
     # sort the indices by the value
-    sort_max_sim = sorted(max_sim, key=lambda x: x[1], reverse=True)
-
+    by_score = sorted(list(enumerate(score)), key=lambda x: x[1], reverse=True)
+    
+    # relation between index in feed and index of closest answered
     closest = [
-        (i, np.where(np.isclose(unans_similarity[i], v))[0][0]) for i, v in sort_max_sim
+        (i, np.where(np.isclose(unans_similarity[i], v))[0][0]) 
+        for i, v in max_sim
     ]
     info = []
-    for u, a in closest:
+    for unans, ans in closest:
         info.append(
-            "Closest: {} ({})".format(
-                user_qa[a][0]["title"], f"{100*max_sim[u][1]:.2f}%"
+            "{} of similarity with {}. Score: {}".format(
+                f"{100*max_sim[unans]:.2f}%",
+                user_qa[ans][0]["title"],
+                f"{100*score[unans]:.2f}"
             )
         )
 
     # get the indexes, now sorted
-    by_max = [x[0] for x in sort_max_sim]
+    by_max = [x[0] for x in by_score]
 
     return by_max, info
